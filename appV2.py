0e06b0e0b36f9bca32d103ec1dbb02845970e6e7
SaktanuthPeak <thanakrit.5122548@gmail.com>
2023-06-09 14:47:38 +0700

    Update appV2.py

--- appV2.py
+++ appV2.py
@@ -1,29 +1,19 @@
 import torch
-
 import urllib
 from PIL import Image
 import streamlit as st
-import urllib.request
 import os
 import os.path as op
-import pandas as pd
-import numpy as np
-from torchvision import datasets, models, transforms
-import torch
-from torch.utils.data import Dataset
-import cv2
+from torchvision import models, transforms
 
 st.title("Thai food image classification")
 st.write("")
-file_up = st.file_uploader("Upload an image", type = "jpg")
+file_up = st.file_uploader("Upload an image", type="jpg")
 transform_test = transforms.Compose([
     transforms.Resize((224, 224), Image.BILINEAR),
     transforms.ToTensor(),
     transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
 ])
-
-
-
 
 model = models.resnet34()
 num_features = model.fc.in_features
@@ -34,13 +24,25 @@ model.load_state_dict(torch.load("Foodimgcls.pth", map_location=torch.device('cp
 model.eval()
 
 # enable users to upload images for the model to make predictions
-img = Image.open(urllib.request.urlopen(file_up))
-scaled_img = transform_test(img)
-torch_images = scaled_img.unsqueeze(0)
+if file_up is not None:
+    # Save the uploaded file to a temporary location
+    temp_file_path = "temp.jpg"
+    with open(temp_file_path, "wb") as f:
+        f.write(file_up.getvalue())
 
-with torch.no_grad():
-    top_n_coordinates, concat_out, raw_logits, concat_logits, part_logits, top_n_index, top_n_prob = model(torch_images)
+    # Open the saved image file using PIL
+    img = Image.open(temp_file_path)
+    scaled_img = transform_test(img)
+    torch_images = scaled_img.unsqueeze(0)
 
-    _, predict = torch.max(concat_logits, 1)
-    pred_id = predict.item()
-    print(' ชนิดอาหาร :', model.food_class[pred_id])
+    with torch.no_grad():
+        top_n_coordinates, concat_out, raw_logits, concat_logits, part_logits, top_n_index, top_n_prob = model(torch_images)
+
+        _, predict = torch.max(concat_logits, 1)
+        pred_id = predict.item()
+        print('ชนิดอาหาร:', model.food_class[pred_id])
+
+    # Remove the temporary image file
+    os.remove(temp_file_path)
+else:
+    st.write("Please upload an image file.")
