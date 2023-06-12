a791e1b823b99c1a6e8e2e2294a72193bba09608
SaktanuthPeak <thanakrit.5122548@gmail.com>
2023-06-12 08:59:12 +0700

    Create scratch_model.py

---
+++ scratch_model.py
@@ -0,0 +1,154 @@
+!pip install torchvision
+!pip install pytorch-lightning
+
+import os
+import os.path as op
+import shutil
+from glob import glob
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from tqdm.auto import tqdm
+
+from torchvision import datasets, models, transforms
+import torchvision.transforms as T
+from torch.utils.data import DataLoader
+from sklearn.model_selection import train_test_split
+
+import torch
+import torch.nn as nn
+from tqdm.auto import tqdm
+import torch.nn.functional as F
+from sklearn.metrics import classification_report, accuracy_score, precision_recall_fscore_support
+
+train_transform = T.Compose([
+    T.Resize((224,224)),
+    T.RandomHorizontalFlip(p=0.2),
+    T.CenterCrop(224),
+    T.ToTensor(),
+    T.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225))
+])
+val_transform = T.Compose([
+    T.Resize((224,224)),
+    T.ToTensor(),
+    T.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225),)
+])
+
+from google.colab import drive
+drive.mount('/content/drive')
+
+import shutil
+source_dir = "/content/drive/MyDrive/10classes.zip"
+destination_dir = "/content"
+shutil.copy(source_dir,destination_dir)
+
+!unzip '/content/dataset.zip' -d '/content/dataset'
+
+train_data = datasets.ImageFolder("/content/dataset/train", transform=train_transform)
+val_data = datasets.ImageFolder("/content/dataset/val", transform=val_transform)
+
+train_loader = DataLoader(train_data, batch_size=32, shuffle=True)
+val_loader = DataLoader(val_data, batch_size=32, shuffle=False)
+
+n_train = len(train_loader.dataset)
+n_val = len(val_loader.dataset)
+n_train,n_val
+
+n_train,n_val
+
+images, labels = next(iter(train_loader))
+
+images.shape, labels.shape
+
+idx2_class = {v: k for k, v in train_data.class_to_idx.items()}
+fig = plt.figure(figsize=(25, 4))
+for i in range(10):
+    image = np.transpose(images.cpu()[i])
+    label = idx2_class[labels.cpu().tolist()[i]]
+    ax = fig.add_subplot(2, 8, i + 1, xticks=[], yticks=[])
+    plt.imshow(image)
+    ax.set_title(label)
+
+cross_entropy = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=2e-3)
+
+gpu = torch.cuda.is_available()
+print(gpu)
+if gpu:
+    model.cuda()
+
+import torch
+import torch.nn as nn
+
+class Food_Model(nn.Module):
+    def __init__(self, num_classes):
+        super(CNN, self).__init__()
+        
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+        
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(64 * 28 * 28, 64),
+            nn.ReLU(),
+            nn.Linear(64, num_classes),
+            nn.Softmax(dim=1)
+        )
+    
+    def forward(self, x):
+        x = self.features(x)
+        x = self.classifier(x)
+        return x
+
+num_classes = 54  
+model = Food_Model(num_classes)
+
+n_epochs = 70
+for epoch in range(n_epochs):
+    model.train()
+    train_loss, val_loss = 0, 0
+    for images, labels in tqdm(train_loader):
+        if gpu:
+            images, labels = images.cuda(), labels.cuda()
+        optimizer.zero_grad()
+        pred = model(images) 
+        loss = cross_entropy(pred, labels)
+        loss.backward() 
+        optimizer.step() 
+        train_loss += loss.item() * images.size(0)
+
+    
+    model.eval() 
+    for images, labels in tqdm(val_loader):
+        if gpu:
+            images, labels = images.cuda(), labels.cuda()
+        pred = model(images)
+        loss = cross_entropy(pred, labels)
+        val_loss += loss.item() * images.size(0)
+    print("Training loss = {}, Validation loss = {}".format(train_loss / n_train, val_loss / n_val))
+
+
+y_pred, y_true = [], []
+model.eval() 
+for images, labels in tqdm(val_loader):
+    if gpu:
+        images, labels = images.cuda(), labels.cuda()
+    pred = model(images)
+    yp = pred.argmax(dim=1).tolist()
+    yt = labels.tolist()
+    y_pred.extend(yp)
+    y_true.extend(yt)
+print(classification_report(y_true, y_pred))
+
+print("Accuracy on validation set = {}".format(
+    accuracy_score(y_true, y_pred))
+)
